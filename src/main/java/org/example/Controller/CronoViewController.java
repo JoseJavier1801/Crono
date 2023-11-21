eaa375b5c80d7560429f3ae2ccac1786f8a6746a
JoseJavier1801 <josejaviergalan2003@gmail.com>
2023-11-21 18:44:30 +0100

    started crono controller

--- src/main/java/org/example/Controller/CronoViewController.java
+++ src/main/java/org/example/Controller/CronoViewController.java
@@ -1,4 +1,33 @@
 package org.example.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class CronoViewController {
+
+    @FXML
+    public Label hours;
+
+    @FXML
+    public Label minutes;
+
+    @FXML
+    public Label seconds;
+
+    @FXML
+    public Button start_btn;
+
+    @FXML
+    public Button stop_btn;
+
+    @FXML
+    public Button restart_btn;
+
+    @FXML
+    public Button save_btn;
+
+
+
+
 }
