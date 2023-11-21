221f9f3d4b50de102c593659614b29a5c1c8e554
alelop <alelopmay@gmail.com>
2023-11-21 23:09:23 +0100

    nuevo

---
+++ src/main/java/org/example/model/time.java
@@ -0,0 +1,57 @@
+package org.example.model;
+
+public class Crono {
+    private int hours;
+    private int minutes;
+    private int seconds;
+    private int total;
+
+    // Constructor vacío
+    public Crono() {
+    }
+
+    // Constructor con parámetros
+    public Crono(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+        // Calcular el total en segundos
+        this.total = hours * 3600 + minutes * 60 + seconds;
+    }
+
+    // Getters y setters
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+        recalculateTotal();
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+        recalculateTotal();
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+        recalculateTotal();
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    private void recalculateTotal() {
+        this.total = hours * 3600 + minutes * 60 + seconds;
+    }
+}
