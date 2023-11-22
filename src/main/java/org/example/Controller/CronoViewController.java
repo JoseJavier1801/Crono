2b709c2ca1cbeac9d96036651760e7ac09d509a2
alelop <alelopmay@gmail.com>
2023-11-22 12:54:56 +0100

    nuevo

---
+++ src/main/java/org/example/Controller/CronoViewController.java
@@ -0,0 +1,90 @@
+package org.example.Controller;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import org.example.App;
+import org.example.Model.Cronometro;
+import org.example.Model.CronometroListener;
+
+import java.io.IOException;
+
+public class CronoViewController implements CronometroListener {
+
+    @FXML
+    public Label hours;
+
+    @FXML
+    public Label minutes;
+
+    @FXML
+    public Label seconds;
+
+    @FXML
+    public Button start_btn;
+
+    @FXML
+    public Button stop_btn;
+
+    @FXML
+    public Button restart_btn;
+
+    @FXML
+    public Button save_btn;
+
+    private Cronometro cronometro;
+    private Thread cronometroThread;
+
+    public CronoViewController() {
+        cronometro = new Cronometro();
+        cronometro.setListener(this); // Establecer el controlador como listener
+        cronometroThread = new Thread(cronometro);
+    }
+
+    @FXML
+    private void Start() {
+        if (!cronometroThread.isAlive()) {
+            cronometro.startCronometro();
+            cronometroThread = new Thread(cronometro);
+            cronometroThread.start();
+
+            // Actualizar los Label con los valores iniciales del cronómetro
+            updateLabels();
+        }
+    }
+
+    @FXML
+    private void Stop() {
+        cronometro.stopCronometro();
+    }
+
+    @FXML
+    private void Restart() {
+        cronometro.stopCronometro();
+        cronometro.resetAndStartCronometro();
+        updateLabels();
+    }
+
+    @Override
+    public void onTimeChanged(int hours, int minutes, int seconds) {
+        Platform.runLater(() -> {
+            this.hours.setText(String.format("%d", hours));
+            this.minutes.setText(String.format("%02d", minutes));
+            this.seconds.setText(String.format("%02d", seconds));
+        });
+    }
+
+    private void updateLabels() {
+        Platform.runLater(() -> {
+            hours.setText(String.format("%d", cronometro.getHours()));
+            minutes.setText(String.format("%02d", cronometro.getMinutes()));
+            seconds.setText(String.format("%02d", cronometro.getSeconds()));
+        });
+    }
+
+    @FXML
+    private void ShowTime() throws IOException {
+        App.setRoot("tablecrono");
+    }
+}
---
+++ src/main/java/org/example/Model/CronometroListener.java
@@ -0,0 +1,5 @@
+package org.example.Model;
+
+public interface CronometroListener {
+    void onTimeChanged(int hours, int minutes, int seconds);
+}
